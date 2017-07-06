1da6a66154871c65a2e410372e566fd846cc9e4e
callisto1337 <hazardous333@gmail.com>
2017-07-06 18:54:27 +0000

    Исправлена ошибка обновления времени последнего поста

--- routes/index.js
+++ routes/index.js
@@ -6,28 +6,28 @@ mongoose.connect('mongodb://localhost/probot');
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
     console.log('db started!');
 });
 
 var userSchema = mongoose.Schema({
     id: {type: Number, unique: true, required: true},
-    lastPost: {type : String}
+    lastPost: {type: String}
 });
 var User = mongoose.model('User', userSchema);
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     methods.app_auth(req, res);
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
     res.send('ok');
     var user_id = req.body.object.from_id;
     var owner_id = req.body.object.owner_id;
     var post_id = req.body.object.id;
     var signed = 1;
-    var access_token = '';
+    var access_token = '212d4d35558ee64e3760ac589601958aa9930b3c81db88acd56677b906e7d9fe16fbd4234fef54dc2ba8f';
 
     if (req.body.type === 'wall_post_new' && req.body.object.post_type === 'suggest') {
         User.find({id: user_id}, function (err, users) {
@@ -36,22 +36,30 @@ router.post('/', function(req, res, next) {
             }
             else {
                 if (users[0] !== undefined) {                                 // Сохранен ли пользователь в базу
-                    if (users[0]['lastPost'] - Date.now() < 3600000) {
-                        methods.post_del(post_id, owner_id, access_token);
+                    if (Date.now() - users[0]['lastPost'] < 3600000) {
+                        setTimeout(function () {
+                            methods.post_del(post_id, owner_id, access_token);
+                        }, 10000)
                     }
                     else {
-                        setTimeout(function() {
-                            Model.findOneAndUpdate({id: user_id}, { lastPost: Date.now() });
-                            methods.post_publish(post_id, owner_id, access_token, signed);
+                        setTimeout(function () {
+                            User.findOneAndUpdate({'id': user_id}, {$set: {'lastPost': Date.now()}}, function (err, page) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                else {
+                                    methods.post_publish(post_id, owner_id, access_token, signed);
+                                }
+                            });
                         }, 30000);
                     }
                 }
                 else {
                     var user = new User({
-                        id:  user_id,
-                        lastPost: Date.now()
+                        'id': user_id,
+                        'lastPost': Date.now()
                     });
-                    setTimeout(function() {
+                    setTimeout(function () {
                         user.save(function (err, fluffy) {
                             if (err) return console.error(err);
                         });
--- routes/methods.js
+++ routes/methods.js
@@ -4,19 +4,23 @@ var fs = require('fs');
 module.exports = {
     post_del(post_id, owner_id, access_token) {
         var url = 'https://api.vk.com/method/wall.delete?post_id=' + post_id + '&owner_id=' + owner_id + '&access_token=' + access_token;
-        setTimeout(function() { request(url) }, 5000);
+        setTimeout(function () {
+            request(url)
+        }, 5000);
     },
 
     post_publish(post_id, owner_id, access_token, signed) {
         var url = 'https://api.vk.com/method/wall.post?post_id=' + post_id + '&owner_id=' + owner_id + '&access_token=' + access_token + '&signed=' + signed;
-        setTimeout(function() { request(url) }, 5000);
+        setTimeout(function () {
+            request(url)
+        }, 5000);
     },
 
     app_auth(req, res) {
         var client_id = 6081626;
         var redirect_uri = 'https://oauth.vk.com/blank.html';
         var scope = 'wall,offline,groups';
-        var url = 'https://oauth.vk.com/authorize?client_id=' + client_id + '&scope=' + scope +'&redirect_uri=' + redirect_uri + '&display=page&response_type=token';
+        var url = 'https://oauth.vk.com/authorize?client_id=' + client_id + '&scope=' + scope + '&redirect_uri=' + redirect_uri + '&display=page&response_type=token';
         res.redirect(url);
     }
 };
